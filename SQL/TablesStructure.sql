3c3e965bef065ffe64bf6251ca88e157a5bbb6e2
PavanKumarBollu <bollupavankumar12@gmail.com>
2026-06-29 15:40:47 +0530

    DATAtypes and table creation

---
+++ SQL/TablesStructure.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS dpba;
+
+
+USE dpba;
+
+
+CREATE TABLE STUDENTS (
+-- structure of the table and fix the datatypes of the column
+name VARCHAR(50) primary key,
+mobile INT(12),
+age int(3)
+);
+
+
