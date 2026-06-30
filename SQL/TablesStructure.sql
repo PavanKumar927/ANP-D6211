2751a58d46736a083973080d0140768d7aae8382
PavanKumarBollu <bollupavankumar12@gmail.com>
2026-06-30 14:55:53 +0530

    InsertINtoTables

--- SQL/TablesStructure.sql
+++ SQL/TablesStructure.sql
@@ -11,4 +11,44 @@ mobile INT(12),
 age int(3)
 );
 
+-- CREATE TABLE table_name (
+ -- columnname1 datatype,
+ -- columnname2 datatype 
+-- );
 
+CREATE TABLE batchdata 
+(
+attendance_date date,
+batchcode VARCHAR(10),
+student_id VARCHAR(12) PRIMARY KEY,
+trainer_mail VARCHAR(20)
+);
+
+-- INTERSTING THE DATA INTO TABLE  
+-- there are so many different ways we can insert the data into table 
+-- based on the requirement we will choose one way and insert the data
+
+INSERT INTO batchdata (
+attendance_date, 
+batchcode, 
+student_id, 
+trainer_mail) 
+VALUES ('2026-06-12', 'ANP-D6211', 'AF05203244', 'bollu@gmail.com');
+
+
+INSERT INTO batchdata ()
+VALUES ('2026-06-12', 'ANP-D6211', 'AF05204426', 'bollu@gmail.com');
+
+INSERT INTO batchdata ()
+VALUES ('ANP-D6211','2026-06-12', 'AF05204426', 'bollu@gmail.com');
+
+INSERT INTO batchdata ()
+VALUES ('2026-06-12','ANP-D6211','bollu@g', 'AF05204426' );
+
+
+INSERT INTO batchdata (
+batchcode, 
+student_id, 
+attendance_date, 
+trainer_mail) 
+VALUES (  'ANP-D6211', 'AF05203255', '2026-06-12','bollu@gmail.com');
