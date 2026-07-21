e9c52b22c194c250a5f2cf328bdadf283d64a66b
PavanKumarBollu <bollupavankumar12@gmail.com>
2026-07-21 15:28:59 +0530

    Python

---
+++ Python/Functions.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    print(a + b)
+    # return a + b;
+
+def mul(a, b):
+    print( a * b);
+
+def div(a, b):
+    print( a / b);
+
+def sub(a , b):
+    print(a - b);
+sub(1,7);
+
+add(10,20);
+mul(2,5);
+div(2.4,5.1);
+
+
+
+
+
+
+# in python we have the spacin that spacing we call it as indentation 
+# def is used for telling python that we are creating
+# functions in python then followed by the function Name
+# () inside brackets you can mention anythin you want to pass 
+# inside that function you can write down anykind of logic you want 
